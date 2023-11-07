d2388e0aaf31fc21059ae3063b79a5ff6a7f0dbd
AYoung Eom <eomssi99@gmail.com>
2023-11-07 16:11:32 +0900

    feat: 비디오 업로드 수정

--- app/src/main/java/com/example/runningcoach_new/VideoFrameExtractor.java
+++ app/src/main/java/com/example/runningcoach_new/VideoFrameExtractor.java
@@ -1,11 +1,15 @@
 package com.example.runningcoach_new;
 
+import static android.content.Intent.getIntent;
+
 import android.content.Context;
+import android.content.Intent;
 import android.content.res.AssetFileDescriptor;
 import android.content.res.AssetManager;
 import android.graphics.Bitmap;
 import android.media.MediaMetadataRetriever;
 import android.net.Uri;
+import android.widget.Toast;
 
 import java.io.File;
 import java.io.IOException;
@@ -16,19 +20,41 @@ public class VideoFrameExtractor {
     private static final int RESIZED_WIDTH = 640; // 리사이즈할 가로 크기
     private static final int RESIZED_HEIGHT = 480; // 리사이즈할 세로 크기
 
-    public static ArrayList<VideoFrame> extractFrames(Context context, AssetManager assetManager) {
+
+//    public static ArrayList<VideoFrame> extractFrames(Context context, AssetManager assetManager) {
+//        MediaMetadataRetriever retriever = new MediaMetadataRetriever();
+//        AssetFileDescriptor assetFileDescriptor;
+//        try {
+//            assetFileDescriptor = assetManager.openFd("videonew.mp4");
+//            retriever.setDataSource(assetFileDescriptor.getFileDescriptor(), assetFileDescriptor.getStartOffset(), assetFileDescriptor.getLength());
+//            assetFileDescriptor.close();
+//        } catch (IOException e){
+//            e.printStackTrace();
+//        }
+
+    public static ArrayList<VideoFrame> extractFrames(Context context, AssetManager assetManager, Uri fileUri) {
+        // fileUri를 사용하여 비디오 파일을 처리
         MediaMetadataRetriever retriever = new MediaMetadataRetriever();
         AssetFileDescriptor assetFileDescriptor;
         try {
-            assetFileDescriptor = assetManager.openFd("videonew.mp4");
-            retriever.setDataSource(assetFileDescriptor.getFileDescriptor(), assetFileDescriptor.getStartOffset(), assetFileDescriptor.getLength());
-            assetFileDescriptor.close();
-        } catch (IOException e){
+            if (context == null)
+                System.out.println("context");
+
+            if (fileUri == null)
+                System.out.println("fileuri");
+            retriever.setDataSource(context, fileUri); // fileUri를 사용하여 데이터 소스 설정
+        } catch (Exception e) {
             e.printStackTrace();
+            System.out.println("비디오 파일을 열 수 없습니다.");
         }
 
 
         String duration = retriever.extractMetadata(MediaMetadataRetriever.METADATA_KEY_DURATION);
+
+        if (duration == null) {
+            // duration이 null이면 비디오 파일의 기간을 가져오지 못한 경우이므로 오류 처리를 수행합니다.
+            System.out.println("비디오 파일의 기간을 가져올 수 없습니다.");
+        }
 
 
         long videoDuration = Long.parseLong(duration);
